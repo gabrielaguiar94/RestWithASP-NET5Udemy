bb02a4d5971b6966e5c3a4a372b1f2989fd36026
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-01-18 18:21:40 -0300

    Finish verbs GET, POST, UPDATE, DELETE

--- 03_RestWithASPNETUdemy_UsingDifferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 03_RestWithASPNETUdemy_UsingDifferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,9 @@ namespace RestWithASPNETUdemy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //Dependecy
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
