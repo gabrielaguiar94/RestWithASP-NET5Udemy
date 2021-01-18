2a48d11b37fd5c859a539c90b7c4636c62c11f22
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-01-18 19:12:05 -0300

    CRUD operations finished

--- 04_RestWithASPNETUdemy_ConnectingToDatabase/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ 04_RestWithASPNETUdemy_ConnectingToDatabase/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -4,68 +4,88 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestWithASPNETUdemy.Model.Context;
 
 namespace RestWithASPNETUdemy.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
-        private volatile int count;
+        private MySqlContext _context;
 
-        public Person Create(Person person)
+        public PersonServiceImplementation(MySqlContext context)
         {
-            return person;
-        }
-
-        public void Delete(long id)
-        {
+            _context = context;
 
         }
 
         public List<Person> FindAll()
         {
-            List<Person> persons = new List<Person>();
-            for (int i = 0; i < 8; i++)
-            {
-                Person person = MockPerson(i);
-                persons.Add(person);
-            }
-            return persons;
+            return _context.Persons.ToList();
         }
 
         public Person FindByID(long id)
         {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Gabriel",
-                LastName = "Aguiar",
-                Address = "São Paulo - Brasil",
-                Gender = "Male"
-
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return person;
+        }
         public Person Update(Person person)
         {
+            if (!Exists(person.Id))
+            {
+                return new Person();
+            }
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
 
             return person;
         }
-        private Person MockPerson(int i)
+        public void Delete(long id)
         {
-            return new Person
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
             {
-                Id = IncrementAndGet() + i,
-                FirstName = "Person Name" + i,
-                LastName = "Person Lastname" + i,
-                Address = "Some Address" + i,
-                Gender = "Male"
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
 
-            };
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
         }
-
-        private long IncrementAndGet()
+        private bool Exists(long id)
         {
-            return Interlocked.Increment(ref count);
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
