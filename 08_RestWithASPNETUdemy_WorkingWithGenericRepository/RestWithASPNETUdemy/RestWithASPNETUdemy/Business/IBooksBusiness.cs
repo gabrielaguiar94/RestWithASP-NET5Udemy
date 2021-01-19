4620ae9e55c89db994eef4dd670a12d1734e6adb
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-01-19 17:20:27 -0300

    Working with GenericRepository

---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IBooksBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IBooksBusiness
+    {
+        Books Create(Books book);
+        Books FindByID(long id);
+        List<Books> FindAll();
+        Books Update(Books book);
+        void Delete(long id);
+    }
+}
---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/BooksBusinessImplementation.cs
@@ -0,0 +1,36 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+using RestWithASPNETUdemy.Repository;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class BooksBusinessImplementation : IBooksBusiness
+    {
+        private readonly IRepository<Books> _repository;
+
+        public BooksBusinessImplementation(IRepository<Books> repository)
+        {
+            _repository = repository;
+        }
+        public List<Books> FindAll()
+        {
+            return _repository.FindAll();
+        }
+        public Books FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+        public Books Create(Books book)
+        {
+            return _repository.Create(book);
+        }
+        public Books Update(Books book)
+        {
+            return _repository.Update(book);
+        }
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
