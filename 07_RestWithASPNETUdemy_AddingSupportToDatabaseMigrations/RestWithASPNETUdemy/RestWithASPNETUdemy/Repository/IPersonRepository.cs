de71740d075b06e4dd6fa946abb11dec2fb4cdfa
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-01-19 11:21:28 -0300

    Adding Support to Database Migrations

---
+++ 07_RestWithASPNETUdemy_AddingSupportToDatabaseMigrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/IPersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
---
+++ 07_RestWithASPNETUdemy_AddingSupportToDatabaseMigrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -0,0 +1,91 @@
+﻿using RestWithASPNETUdemy.Model;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWithASPNETUdemy.Model.Context;
+
+namespace RestWithASPNETUdemy.Repository.Implementations
+{
+    public class PersonRepositoryImplementation : IPersonRepository
+    {
+        private MySqlContext _context;
+
+        public PersonRepositoryImplementation(MySqlContext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public Person FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return person;
+        }
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id))
+            {
+                return null;
+            }
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+            return person;
+        }
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+        public bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}
