c12e07eab0e65b31b4275df63312521f878c8f51
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-01-19 19:31:59 -0300

    Custom Serialization

---
+++ 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BooksVO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class BooksVO
+    {
+        [JsonPropertyName("id_book")]
+        public long Id { get; set; }
+        public string Author { get; set; }
+        [JsonIgnore]
+        public DateTime LaunchedDate { get; set; }
+        public double Price { get; set; }
+        public string Title { get; set; }
+    }
+}
