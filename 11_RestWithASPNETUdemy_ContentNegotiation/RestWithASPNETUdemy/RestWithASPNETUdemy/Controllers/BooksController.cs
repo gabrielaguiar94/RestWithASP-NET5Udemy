e5e175506c0a9219f923ca187be1760d47a0353e
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-01-20 18:38:45 -0300

    Content negotiation (xml)

---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+        private IBooksBusiness _bookBusiness;
+        public BooksController(ILogger<BooksController> logger, IBooksBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(_bookBusiness.FindAll());
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+
+            var book = _bookBusiness.FindByID(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] BooksVO book)
+        {
+
+            if (book == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_bookBusiness.Create(book));
+        }
+        [HttpPut]
+        public IActionResult Put([FromBody] BooksVO book)
+        {
+
+            if (book == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_bookBusiness.Update(book));
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+
+    }
+
+}
