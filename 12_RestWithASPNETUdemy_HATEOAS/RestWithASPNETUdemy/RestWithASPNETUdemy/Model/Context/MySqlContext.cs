21c46d41844a77701254da494bb724c27773f9b0
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-01-21 09:59:09 -0300

    Add HATEOAS

---
+++ 12_RestWithASPNETUdemy_HATEOAS/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Context/MySqlContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNETUdemy.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext()
+        {
+
+        }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Books> Books { get; set; }
+    }
+}
