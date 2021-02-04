89b93da091d66be113900d887d1c82f6723f00a7
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-02-04 10:34:57 -0300

    Delete 19_RestWithASPNETUdemy_AddingDockerSupport directory
    Delete to create again and fix problems

--- 19_RestWithASPNETUdemy_AddingDockerSupport/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-	(1, 'São Paulo - Brasil', 'Gabriel', 'Male', 'Aguiar'),
-	(2, 'São Paulo', 'Cida', 'Female', 'Oliveira'),
-	(3, 'São Paulo', 'Rafaela', 'Female', 'Aguiar');
