af2a889d580c331c91184ee84553bd02e5564a00
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-02-04 14:20:27 -0300

    Adding Docker Support

---
+++ 19_RestWithASPNETUdemy_AddingDockerSupport/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Filters;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Authorize("Bearer")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+        private IBooksBusiness _bookBusiness;
+        public BooksController(ILogger<BooksController> logger, IBooksBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+
+            return Ok(_bookBusiness.FindAll());
+        }
+        [HttpGet("{id}")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+
+            var book = _bookBusiness.FindByID(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+        [HttpPost]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] BooksVO book)
+        {
+
+            if (book == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_bookBusiness.Create(book));
+        }
+        [HttpPut]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] BooksVO book)
+        {
+
+            if (book == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_bookBusiness.Update(book));
+        }
+        [HttpDelete("{id}")]        
+        public IActionResult Delete(long id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
